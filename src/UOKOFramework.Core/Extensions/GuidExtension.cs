6a91dff60963325f0701030dc56904ee11ef8138
chunqiu <chunqiu@uoko.com>
2017-09-08 13:51:00 +0800

    添加GuidExtension.cs

---
+++ src/UOKOFramework.Core/Extensions/GuidExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UokoFramework.Core.Extensions
+{
+    /// <summary>
+    /// Guid的扩展方法
+    /// </summary>
+    public static class GuidExtension
+    {
+        /// <summary>
+        /// 转换为String数组
+        /// </summary>
+        /// <param name="guids"></param>
+        /// <param name="format">默认"N",["D", "N", "B", "P", "X"]</param>
+        /// <returns></returns>
+        public static string[] ToStringArray(this IEnumerable<Guid> guids, string format = "N")
+        {
+            if (guids == null)
+            {
+                return new string[0];
+            }
+            return guids.Select(id => id.ToString(format)).ToArray();
+        }
+    }
+}
---
+++ test/UOKOFramework.Core.Test/Extensions/GridExtension/ToStringArrayTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UokoFramework.Core.Extensions;
+using Xunit;
+
+namespace UokoFramework.Core.Test.Extensions.GridExtension
+{
+    public class ToStringArrayTest
+    {
+        [Fact]
+        public void when_guids_is_null()
+        {
+            var guids = (IEnumerable<Guid>)null;
+
+            var stringArray = guids.ToStringArray();
+
+            Assert.Equal(0, stringArray.Length);
+        }
+
+        [Fact]
+        public void when_guids_is_empty()
+        {
+            var guids = new Guid[0];
+
+            var stringArray = guids.ToStringArray();
+
+            Assert.Equal(0, stringArray.Length);
+        }
+
+        [Fact]
+        public void when_guids_is_vaild()
+        {
+            var guids = new List<Guid>
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            };
+
+            var stringArray = guids.ToStringArray();
+
+            for (var i = 0; i < guids.Count; i++)
+            {
+                Assert.Equal(guids[i].ToString("N"), (stringArray[i]));
+            }
+
+        }
+    }
+}
