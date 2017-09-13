9e17a18913bb6bf787522fe0e51a03e480589095
chunqiu <chunqiu@uoko.com>
2017-09-13 15:15:39 +0800

    添加StringExtension和ByteExtension.

---
+++ src/UOKOFramework/Extensions/ByteExtension.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UokoFramework.Extensions
+{
+    /// <summary>
+    /// Byte的扩展方法
+    /// </summary>
+    public static class ByteExtension
+    {
+        /// <summary>
+        /// 获取散列值
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <param name="hashAlgorithm">具体的hash算法</param>
+        /// <returns>hash字节数组</returns>
+        private static byte[] GetHash(this byte[] bytes, HashAlgorithm hashAlgorithm)
+        {
+            if (bytes == null || bytes.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+            if (hashAlgorithm == null)
+            {
+                throw new ArgumentNullException(nameof(hashAlgorithm));
+            }
+            using (hashAlgorithm)
+            {
+                return hashAlgorithm.ComputeHash(bytes);
+            }
+        }
+
+        /// <summary>
+        /// 获取MD5
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <returns>16byte的数组</returns>
+        // ReSharper disable once InconsistentNaming
+        public static byte[] GetMD5(this byte[] bytes)
+        {
+            return bytes.GetHash(MD5.Create());
+        }
+
+        /// <summary>
+        /// 获取SHA1
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <returns>20byte的数组</returns>
+        // ReSharper disable once InconsistentNaming
+        public static byte[] GetSHA1(this byte[] bytes)
+        {
+            return bytes.GetHash(SHA1.Create());
+        }
+
+        /// <summary>
+        /// 获取SHA256
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <returns>32byte的数组</returns>
+        // ReSharper disable once InconsistentNaming
+        public static byte[] GetSHA256(this byte[] bytes)
+        {
+            return bytes.GetHash(SHA256.Create());
+        }
+
+        /// <summary>
+        /// bytes转16进制
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <param name="withHyphen">使用分隔符"-"分割，默认true。</param>
+        /// <param name="lowerCase">小写，默认false。</param>
+        /// <returns>16进制的字符串</returns>
+        public static string GetHex(this byte[] bytes, bool withHyphen = true, bool lowerCase = false)
+        {
+            if (bytes == null || bytes.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+            var hexString = BitConverter.ToString(bytes);
+
+            if (withHyphen == false)
+            {
+                hexString = hexString.Replace("-", "");
+            }
+
+            if (lowerCase == true)
+            {
+                hexString = hexString.ToLower();
+            }
+
+            return hexString;
+        }
+
+        /// <summary>
+        /// bytes转base64
+        /// </summary>
+        /// <param name="bytes">原始byte数组</param>
+        /// <returns>base64字符串</returns>
+        public static string GetBase64(this byte[] bytes)
+        {
+            if (bytes == null || bytes.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// bytes转字符串
+        /// </summary>
+        /// <param name="bytes">原始字符串</param>
+        /// <param name="encoding">编码格式，默认采用UTF8编码</param>
+        /// <returns></returns>
+        public static string GetString(this byte[] bytes, Encoding encoding = null)
+        {
+
+            if (bytes == null || bytes.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
+            if (encoding == null)
+            {
+                encoding = Encoding.UTF8;
+            }
+            return encoding.GetString(bytes);
+        }
+    }
+}
---
+++ test/UOKOFramework.Test/Extensions/ByteExtension/GetMD5Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UokoFramework.Extensions;
+using Xunit;
+
+namespace UokoFramework.Test.Extensions.ByteExtension
+{
+    public class GetMD5Test
+    {
+        [Fact]
+        public void when_bytes_is_null_should_throw_ArgumentNullException()
+        {
+            var bytes = (byte[])null;
+
+            Assert.Throws<ArgumentNullException>(() => bytes.GetMD5());
+        }
+    }
+}
