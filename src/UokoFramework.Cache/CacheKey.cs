498ad6f09299b51b2f9a766c2d40e33e0d764956
chunqiu <chunqiu@uoko.com>
2017-09-13 11:15:43 +0800

    添加CacheKey.cs。

---
+++ src/UokoFramework.Cache/CacheKey.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UokoFramework.Cache
+{
+    /// <summary>
+    /// CacheKey由三部分组成。
+    /// <para>1. scope : 表示cache的应用范围</para>
+    /// <para>2. params: 表示构造key所需的动态参数</para>
+    /// <para>3. name  : 表示构造key所需的固定部分的名字</para>
+    /// <para>例子:user:&amp;user-id=xxx#profile</para>
+    /// </summary>
+    public class CacheKey
+    {
+        private readonly string _scope;
+
+        private IDictionary<string, string> _params;
+
+        private string _name;
+
+        
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="scope">key的scope</param>
+        protected CacheKey(string scope)
+        {
+            if (string.IsNullOrWhiteSpace(scope))
+            {
+                throw new ArgumentNullException(nameof(scope));
+            }
+            _scope = scope.ToLower();
+        }
+
+        /// <summary>
+        /// 克隆key
+        /// </summary>
+        /// <param name="name">key的固定部分的名字</param>
+        /// <returns>克隆后的新CacheKey</returns>
+        protected CacheKey Clone(string name)
+        {
+            var clonedCacheKey = (CacheKey)MemberwiseClone();
+            clonedCacheKey._name = name.Trim();
+            if (_params != null)
+            {
+                clonedCacheKey._params = new Dictionary<string, string>(_params.Count);
+                foreach (var param in _params)
+                {
+                    clonedCacheKey._params.Add(param);
+                }
+            }
+            return clonedCacheKey;
+        }
+
+        /// <summary>
+        /// 构造key
+        /// </summary>
+        /// <typeparam name="TCacheKey">具体的key的类型</typeparam>
+        /// <param name="key">动态参数的key</param>
+        /// <param name="value">动态参数的value</param>
+        /// <returns>当前CacheKey自身</returns>
+        protected TCacheKey Build<TCacheKey>(string key, string value) 
+            where TCacheKey : CacheKey
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            _params = new Dictionary<string, string>()
+            {
+                [key] = value
+            };
+
+            return (TCacheKey)this;
+        }
+
+        /// <summary>
+        /// 构造key
+        /// </summary>
+        /// <typeparam name="TCacheKey">具体的key的类型</typeparam>
+        /// <param name="keyValues">动态参数集合</param>
+        /// <returns></returns>
+        protected TCacheKey Build<TCacheKey>(IDictionary<string, string> keyValues) 
+            where TCacheKey : CacheKey
+        {
+            if (keyValues == null)
+            {
+                throw new ArgumentNullException(nameof(keyValues));
+            }
+
+            _params = keyValues;
+
+            return (TCacheKey)this;
+        }
+
+        /// <summary>
+        /// 转换成字符串形式的key，结构如下：
+        /// <para>scope:&amp;param1=value1&amp;param2=value2#name</para>
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            if (_name == null)
+            {
+                throw new ArgumentNullException(nameof(_name));
+            }
+
+            var keyBuilder = new StringBuilder(_scope);
+            keyBuilder.Append(':');
+
+            if (_params != null)
+            {
+                foreach (var param in _params)
+                {
+                    keyBuilder.Append('&');
+                    keyBuilder.Append(param.Key);
+                    keyBuilder.Append('=');
+                    keyBuilder.Append(param.Value);
+                }
+            }
+
+            keyBuilder.Append('#');
+            keyBuilder.Append(_name);
+
+            return keyBuilder.ToString();
+        } 
+    }
+}
---
+++ test/UokoFramework.Cache.Test/CacheKeyTest.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UokoFramework.Cache.Test
+{
+
+    public class CacheKeyTest
+    {
+        public class SubCacheKey : CacheKey
+        {
+            // 正常情况下应该是privated的只读的常量，这里设置为外界可写为了测试。
+            public static string Scope;
+
+            // 正常情况下应该是private，这里设置为public是为了更直观的测试。
+            public class Params
+            {
+                // 正常情况下应该是只读的常量，这里设置为外界可写为了更直观的测试。
+                public static string UserId;
+                public static string ClientId;
+            }
+
+            // 正常情况下应该是private，这里设置为public是为了更直观的测试。
+            public class Names
+            {
+                // 正常情况下应该是只读的常量，这里设置为外界可写为了更直观的测试。
+                public static string Empty;
+                public static string Apps;
+                public static string Profile;
+            }
+
+            public SubCacheKey() : base(Scope)
+            {
+
+            }
+
+            public SubCacheKey Build(string userId)
+            {
+                return base.Build<SubCacheKey>(Params.UserId, userId);
+            }
+
+            public SubCacheKey Build(string userId, string clientId)
+            {
+                return base.Build<SubCacheKey>(new Dictionary<string, string>
+                {
+                    [Params.UserId] = userId,
+                    [Params.ClientId] = clientId,
+                });
+            }
+
+            public CacheKey Profile => base.Clone(Names.Profile);
+
+            public CacheKey Apps => base.Clone(Names.Apps);
+
+            public CacheKey Empty => base.Clone(Names.Empty);
+        }
+
+        [Fact]
+        public void should_throw_ArgumentNullException_when_not_set_name()
+        {
+            SubCacheKey.Scope = "test";
+            var subCacheKey = new SubCacheKey();
+
+            Assert.Throws<ArgumentNullException>(() => subCacheKey.ToString());
+        }
+
+        [Fact]
+        public void should_throw_ArgumentNullException_when_only_call_build_but_not_set_name()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            const string userId = "123";
+            var subCacheKey = new SubCacheKey();
+
+            var cacheKey = subCacheKey.Build(userId);
+
+            Assert.Throws<ArgumentNullException>(() => cacheKey.ToString());
+        }
+
+        [Fact]
+        public void should_throw_ArgumentNullException_when_call_subClass_ToString_after_set_name()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            SubCacheKey.Names.Profile = "profile";
+            const string userId = "123";
+            var subCacheKey = new SubCacheKey().Build(userId);
+
+            var cacheKey = subCacheKey.Profile;
+
+            Assert.Throws<ArgumentNullException>(() => subCacheKey.ToString());
+            Assert.Equal("test:&user-id=123#profile", cacheKey.ToString());
+        }
+
+        [Fact]
+        public void should_no_include_name_when_name_is_empty()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Names.Empty = string.Empty;
+            var cacheKey = new SubCacheKey().Empty;
+
+            var key = cacheKey.ToString();
+
+            Assert.Equal("test:#", key);
+        }
+
+        [Fact]
+        public void should_get_key_include_param_and_name_when_has_param_and_has_name()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            SubCacheKey.Names.Apps = "apps";
+            const string userId = "123";
+            var cacheKey = new SubCacheKey().Build(userId).Apps;
+
+            var key = cacheKey.ToString();
+
+            Assert.Equal("test:&user-id=123#apps", key);
+        }
+
+        [Fact]
+        public void should_use_last_build_param_when_call_twice_build()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            SubCacheKey.Params.ClientId = "client-id";
+            SubCacheKey.Names.Apps = "apps";
+            const string userId = "123";
+            const string clientId = "abc";
+            var subCacheKey = new SubCacheKey();
+
+            var cacheKey = subCacheKey.Build(userId, clientId).Build(userId).Apps;
+
+            Assert.Equal("test:&user-id=123#apps", cacheKey.ToString());
+        }
+
+        [Fact]
+        public void should_use_last_name_when_set_twice_name()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            SubCacheKey.Names.Apps = "apps";
+            SubCacheKey.Names.Profile = "profile";
+            var subCacheKey = new SubCacheKey();
+
+            var cacheKey = ((SubCacheKey)subCacheKey.Apps).Profile;
+
+            Assert.Equal("test:#profile", cacheKey.ToString());
+        }
+
+        [Fact]
+        public void should_get_right_sequencekey_key_when_call_build_after_set_name()
+        {
+            SubCacheKey.Scope = "test";
+            SubCacheKey.Params.UserId = "user-id";
+            SubCacheKey.Names.Apps = "apps";
+            const string userId = "123";
+            var subCacheKey = new SubCacheKey();
+
+            var cacheKey = ((SubCacheKey)subCacheKey.Apps).Build(userId);
+
+            Assert.Equal("test:&user-id=123#apps", cacheKey.ToString());
+        }
+    }
+}
