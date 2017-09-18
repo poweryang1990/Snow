040835054e4863c3e9d79603b677bc3ace82ed39
chunqiu <chunqiu@uoko.com>
2017-09-18 11:53:31 +0800

    添加WhereIf扩展方法。

---
+++ src/UOKOFramework/Extensions/EnumerableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UOKOFramework.Extensions
+{
+    /// <summary>
+    /// Enumerable的扩展方法
+    /// </summary>
+    public static class EnumerableExtension
+    {
+        /// <summary>
+        /// 在condition为true的情况下应用Where表达式
+        /// </summary>
+        /// <typeparam name="T">类型参数</typeparam>
+        /// <param name="queryable">this</param>
+        /// <param name="condition">条件</param>
+        /// <param name="predicate">Where表达式</param>
+        /// <returns>IEnumerable</returns>
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> queryable,
+            bool condition,
+            Func<T, bool> predicate)
+        {
+            return condition ? queryable.Where(predicate) : queryable;
+        }
+    }
+}
---
+++ src/UOKOFramework/Extensions/QuaryableExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace UOKOFramework.Extensions
+{
+    /// <summary>
+    /// Quaryable的扩展方法
+    /// </summary>
+    public static class QuaryableExtension
+    {
+        /// <summary>
+        /// 在condition为true的情况下应用Where表达式
+        /// </summary>
+        /// <typeparam name="T">类型参数</typeparam>
+        /// <param name="queryable">this</param>
+        /// <param name="condition">条件</param>
+        /// <param name="predicate">Where表达式</param>
+        /// <returns>IQueryable</returns>
+        public static IQueryable<T> WhereIf<T>(this IQueryable<T> queryable,
+            bool condition,
+            Expression<Func<T, bool>> predicate)
+        {
+            return condition ? queryable.Where(predicate) : queryable;
+        }
+    }
+}
