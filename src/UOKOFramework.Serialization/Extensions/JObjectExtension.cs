4b0d001ebc373285ee0c3fcf771f293a652e184f
chunqiu <chunqiu@uoko.com>
2017-09-20 12:20:11 +0800

    添加Object.ToJSON和JObject.GetProperty扩展方法。

---
+++ src/UOKOFramework.Serialization/Extensions/JObjectExtension.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+
+namespace UOKOFramework.Serialization.Extensions
+{
+    /// <summary>
+    /// JObject的扩展方法
+    /// </summary>
+    public static class JObjectExtension
+    {
+        /// <summary>
+        /// 获取指定的属性
+        /// </summary>
+        /// <param name="jsonObject">this</param>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="stringComparison">name是否忽略大小写</param>
+        /// <returns>指定的属性</returns>
+        public static JToken GetProperty(
+            this JObject jsonObject,
+            string propertyName,
+            StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
+        {
+            if (jsonObject == null || string.IsNullOrWhiteSpace(propertyName))
+            {
+                return null;
+            }
+
+            if (jsonObject.TryGetValue(propertyName, stringComparison, out var jtoken))
+            {
+                return jtoken;
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 获取指定的属性，转为sting类型。
+        /// </summary>
+        /// <param name="jsonObject">this</param>
+        /// <param name="propertyName">属性名</param>
+        /// <param name="stringComparison">name是否忽略大小写</param>
+        /// <returns>指定的属性的字符串值</returns>
+        public static string GetStringProperty(
+            this JObject jsonObject,
+            string propertyName,
+            StringComparison stringComparison = StringComparison.OrdinalIgnoreCase)
+        {
+            var property = jsonObject.GetProperty(propertyName, stringComparison);
+            if (property == null)
+            {
+                return string.Empty;
+            }
+            return property.ToString();
+        }
+    }
+}
---
+++ test/UOKOFramework.Serialization.Test/Extensions/JObjectExtension/GetStringPropertyTest.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using Xunit;
+using UOKOFramework.Serialization.Extensions;
+
+namespace UOKOFramework.Serialization.Test.Extensions.JObjectExtension
+{
+    // ReSharper disable once InconsistentNaming
+    public class GetStringPropertyTest
+    {
+        [Fact]
+        public void when_jobject_is_null_should_return_string_empty()
+        {
+            JObject jObject = null;
+
+            Assert.Equal(string.Empty, jObject.GetStringProperty("a"));
+        }
+
+        [Fact]
+        public void when_property_name_is_not_exist_should_return_string_empty()
+        {
+            var jObject = JObject.Parse("{}");
+
+            Assert.Equal(string.Empty, jObject.GetStringProperty("a"));
+        }
+
+        [Fact]
+        public void when_jobject_and_property_name_is_valid()
+        {
+            var jObject = JObject.Parse("{a:123}");
+
+            Assert.Equal("123", jObject.GetStringProperty("a"));
+            Assert.Equal("123", jObject.GetStringProperty("A"));
+        }
+    }
+}
