a406c8906dddc183a2dfef1a2b24f4c6ff8d79c5
chunqiu <chunqiu@uoko.com>
2017-09-28 15:47:12 +0800

    添加UOKOFramework.IPDB。

---
+++ test/UOKOFramework.IPDB.Test/IPv4DBTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+// ReSharper disable InconsistentNaming
+
+namespace UOKOFramework.IPDB.Test
+{
+    public class IPv4DBTest
+    {
+        [Fact]
+        public void Find()
+        {
+            var ipResult = IPv4DB.Find("113.208.112.114");
+            Assert.Equal("中国", ipResult[0]);
+            Assert.Equal("北京", ipResult[1]);
+            Assert.Equal("北京", ipResult[2]);
+        }
+    }
+}
