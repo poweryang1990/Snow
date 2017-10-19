1c797a8839ad144aa27d226fa0bc6bd8e2f2ee3a
chunqiu <chunqiu@uoko.com>
2017-10-19 16:35:10 +0800

    添加IMessageQuene接口。

---
+++ src/Snow.MQ/MessageQuene.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace Snow.MQ
+{
+    /// <summary>
+    /// RabbitMQ
+    /// </summary>
+    public class MessageQuene : IMessageQuene
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="options"></param>
+        public MessageQuene(MessageQueneOptions options)
+        {
+            Throws.ArgumentNullException(options, nameof(options));
+            Throws.ArgumentNullException(options.HostName, nameof(options.HostName));
+            Throws.ArgumentNullException(options.UserName, nameof(options.UserName));
+            Throws.ArgumentNullException(options.Password, nameof(options.Password));
+
+            this._connectionFactory = new ConnectionFactory
+            {
+                HostName = options.HostName,
+                UserName = options.UserName,
+                Password = options.Password,
+                AutomaticRecoveryEnabled = options.AutomaticRecoveryEnabled,
+                NetworkRecoveryInterval = options.NetworkRecoveryInterval
+            };
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IModel CreateChannel()
+        {
+            if (this._connection == null)
+            {
+                this._connection = this._connectionFactory.CreateConnection();
+            }
+            var channel = this._connection.CreateModel();
+            this._connection.AutoClose = true;
+            return channel;
+        }
+    }
+}
---
+++ src/Snow.MQ/MessageQueneOptions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Snow.MQ
+{
+    /// <summary>
+    /// Message Quene配置信息
+    /// </summary>
+    public class MessageQueneOptions
+    {
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 自动恢复连接。默认为true。
+        /// </summary>
+        public bool AutomaticRecoveryEnabled { get; set; } = true;
+
+        /// <summary>
+        /// 恢复连接重试事件间隔，单位秒。默认3秒。
+        /// </summary>
+        public TimeSpan NetworkRecoveryInterval { get; set; } = TimeSpan.FromSeconds(3);
+
+    }
+}
