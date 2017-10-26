fba29e4d6537902962ce1c8acb91cc3ee5ffd0c8
chunqiu <chunqiu@uoko.com>
2017-10-26 09:39:54 +0800

    Feedback - 重命名queryable为source。

--- src/Snow/Extensions/EnumerableExtension.cs
+++ src/Snow/Extensions/EnumerableExtension.cs
@@ -13,15 +13,15 @@ namespace Snow.Extensions
         /// 在condition为true的情况下应用Where表达式
         /// </summary>
         /// <typeparam name="T">类型参数</typeparam>
-        /// <param name="queryable">this</param>
+        /// <param name="source">this</param>
         /// <param name="condition">条件</param>
         /// <param name="predicate">Where表达式</param>
         /// <returns>IEnumerable</returns>
-        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> queryable,
+        public static IEnumerable<T> WhereIf<T>(this IEnumerable<T> source,
             bool condition,
             Func<T, bool> predicate)
         {
-            return condition ? queryable.Where(predicate) : queryable;
+            return condition ? source.Where(predicate) : source;
         }
 
         /// <summary>
