a2ec7b2703a91332f7829909b5e89d2f2cf2608b
chunqiu <chunqiu@uoko.com>
2017-10-31 14:21:44 +0800

    #4860 拆分IDistributedCache接口到多个独立的文件。

---
+++ src/Snow.Cache/IDistributedCache.Increment.cs
@@ -0,0 +1,21 @@
+﻿namespace Snow.Cache
+{
+    public partial interface IDistributedCache
+    {
+        /// <summary>
+        /// 自增
+        /// </summary>
+        /// <param name="key">缓存的键</param>
+        /// <param name="value"></param>
+        /// <returns>是否自增成功</returns>
+        long Increment(CacheKey key, long value = 1);
+
+        /// <summary>
+        /// 自减
+        /// </summary>
+        /// <param name="key">缓存的键</param>
+        /// <param name="value"></param>
+        /// <returns>是否自减成功</returns>
+        long Decrement(CacheKey key, long value = 1);
+    }
+}
